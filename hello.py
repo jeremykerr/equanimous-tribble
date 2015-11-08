255ca51f4f8255bb33ece00618fb86b85ba8a2da
Jeremy Kerr <jeremy.p.kerr@gmail.com>
2015-11-07 20:18:42 -0700

    Added hello world application.

---
+++ hello.py
@@ -0,0 +1,15 @@
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+  return "Hello, world."
+
+if __name__ == "__main__":
+  # Debug should be set to False on production machines. Setting Debug mode to True allows for a security
+  # issue that can be used to execute arbitrary code. See the Flask documentation for more information.
+  app.debug = False
+  app.run()
+
+
